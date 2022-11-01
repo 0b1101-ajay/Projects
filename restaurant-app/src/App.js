a9bb6efd5e0fa41e71701a55e38f47980843b6e2
0b1101-ajay <102550795+0b1101-ajay@users.noreply.github.com>
2022-11-01 22:53:50 +0530

    Add files via upload

---
+++ restaurant-app/src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import "./components/style.css";
+import Menu from "./components/menuApi"
+import MenuCard from "./components/MenuCard";
+import Navbar from "./components/Navbar";
+
+const uniqueList = [
+  ...new Set(
+    Menu.map((curElem) => {
+      return curElem.category;
+    })
+  ),
+  "All",
+];
+
+
+const App = () => {
+  const [menuData, setMenuData] = useState(Menu);
+  const [menuList, setMenuList] = useState(uniqueList);
+
+  const filterItem = (category) => {
+    if (category === "All") {
+      setMenuData(Menu);
+      return;
+    }
+
+    const updatedList = Menu.filter((curElem) => {
+      return curElem.category === category;
+    });
+
+    setMenuData(updatedList);
+  };
+
+  return (
+    <>
+      <Navbar filterItem={filterItem} menuList={menuList} />
+      <MenuCard menuData={menuData} />
+    </>
+  );
+};
+
+export default App;
